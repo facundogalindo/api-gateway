5d59cd7e25f83de9681bbb342c4358adb5cbef36
Facundo Galindo <galindofacundo@gmail.com>
2025-11-07 11:05:10 -0300

    Inicializa proyecto api-gateway

---
+++ src/test/java/utn/frc/bda/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package utn.frc.bda.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
